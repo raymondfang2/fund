0a287b3ada30290e297cd446ac501b71fefb0395
Fang Limin <fanglimin@Fangs-MacBook-Air.local>
2021-11-19 18:18:03 +0800

    bug fix in smart contract

--- client/src/factory.js
+++ client/src/factory.js
@@ -3,7 +3,7 @@ import FundFactory from "./contracts/FundFactory.json";
 
 const instance = new web3.eth.Contract(
   FundFactory.abi,
-  "0x51Db7f91171dCc3508a82e8FDBaAa364f071FF5E"
+  "0xD34fdF127573241FBF69A598F82045DF870a9640"
 );
 //replace the above address with the output of "truffle migrate ..." --> the contract address of FundFactory
 //Only the account deployed the above factory can raise fund --> the charityOwner
